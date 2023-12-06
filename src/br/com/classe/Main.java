66d473b9e2005163d2524b1513c0596cf13a309d
Felipe Batista <feljava32@gmail.com>
2023-12-06 16:27:24 -0300

    Tarefa tirar media

---
+++ src/br/com/classe/Main.java
@@ -0,0 +1,14 @@
+package br.com.classe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Prova prova = new Prova();
+		
+		prova.media(6, 8, 9, 3);
+		
+            
+	}
+
+}
---
+++ src/br/com/classe/Prova.java
@@ -0,0 +1,21 @@
+package br.com.classe;
+
+public class Prova {
+
+	public int nota1;
+	public int nota2;
+	public int nota3;
+	public int nota4;
+	
+	
+	 public void media(int nota1,int nota2,int nota3,int nota4) {
+		 int media = (nota1+nota2+nota3+nota4)/4;
+		   
+		    System.out.println("Sua media no semestre é:"+media);
+	 }
+	  
+	    
+	    
+	    
+	 
+	}; 
