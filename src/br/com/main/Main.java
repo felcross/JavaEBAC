57852de23833b46bbdc76110b36b5524ca64471d
Felipe Batista <feljava32@gmail.com>
2023-12-13 16:35:01 -0300

    part1 da tarefa

--- src/br/com/main/Main.java
+++ src/br/com/main/Main.java
@@ -36,7 +36,7 @@ public class Main {
 			System.out.println("Voce está Reprovado");
 		}
 	
- 
+   
 		
 	
 	  
---
+++ src/br/com/main/Main2.java
@@ -0,0 +1,46 @@
+package br.com.main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import br.com.media.Prova;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		//String[] lista;
+		
+		System.out.println("Insira nomes para lista");
+		
+		System.out.println("Insira Nomes");
+		String nomes =  scanner.nextLine();
+		String[] lista = nomes.split(",");
+		
+		for (String nome : lista) {
+            System.out.println(nome);
+        }
+		
+		
+	
+		 
+		
+
+	   
+	   
+		
+	
+   
+		
+	
+	  
+	 
+	
+	
+	
+	}
+	
+
+}
